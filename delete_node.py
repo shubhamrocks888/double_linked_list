21ed114511a20c28acd3c8dd8a179a520655e8bf
unknown <shubhamrocks888@gmail.com>
2020-06-11 10:02:21 +0530

    delete a node

--- delete_node.py
+++ delete_node.py
@@ -1,3 +1,6 @@
+# for Garbage collection 
+import gc 
+
 class node:
     def __init__(self,data=None):
         self.data = data
@@ -19,6 +22,31 @@ class dll:
             new_node.next = self.head
             self.head.prev = new_node
             self.head = new_node
+
+    def deleteNode(self, dele): 
+          
+        # Base Case 
+        if self.head is None or dele is None: 
+            return 
+          
+        # If node to be deleted is head node 
+        if self.head == dele: 
+            self.head = dele.next
+  
+        # Change next only if node to be deleted is NOT 
+        # the last node 
+        if dele.next is not None: 
+            dele.next.prev = dele.prev 
+      
+        # Change prev only if node to be deleted is NOT  
+        # the first node 
+        if dele.prev is not None: 
+            dele.prev.next = dele.next
+        # Finally, free the memory occupied by dele 
+        # Call python garbage collector 
+        gc.collect() 
+  
+
 
     def delete_node(self,data):
         cur_node = self.head
@@ -63,3 +91,27 @@ l.push(2)
 l.push(1)
 l.delete_node(3)
 l.print_list()
+
+
+# Driver program to test the above functions 
+  
+# Start with empty list 
+dll = DoublyLinkedList() 
+  
+# Let us create the doubly linked list 10<->8<->4<->2 
+dll.push(2); 
+dll.push(4); 
+dll.push(8); 
+dll.push(10); 
+  
+print "\n Original Linked List", 
+dll.printList(dll.head) 
+  
+# delete nodes from doubly linked list 
+dll.deleteNode(dll.head) 
+dll.deleteNode(dll.head.next) 
+dll.deleteNode(dll.head.next) 
+# Modified linked list will be NULL<-8->NULL 
+print "\n Modified Linked List", 
+dll.printList(dll.head) 
+  
